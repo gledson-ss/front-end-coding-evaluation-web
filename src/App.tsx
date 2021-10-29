0ba75e60794709df18a5578cbabb70ea0c17e793
gledson-ss <github@gledson.dev>
2021-10-29 01:28:16 -0300

    feat: add Providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
 import { ThemeProvider } from 'styled-components';
+import { ModalProvider } from 'styled-react-modal';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import { theme } from './styles/theme';
 
 const App: FC = () => (
   <ThemeProvider theme={theme}>
-    <Routes />
-    <GlobalStyle />
+    <ModalProvider>
+      <Routes />
+      <GlobalStyle />
+    </ModalProvider>
   </ThemeProvider>
 );
 
