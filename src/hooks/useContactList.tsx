55c5e5c14b2517cb219855f9d6b6b2c0f7da22ae
gledson-ss <github@gledson.dev>
2021-10-31 12:05:28 -0300

    feat: update methods

--- src/hooks/useContactList.tsx
+++ src/hooks/useContactList.tsx
@@ -1,31 +1,55 @@
 import create from 'zustand';
 
-interface adressProps {
-  cep: string;
-  localidade: string;
-  uf: string;
-  logradouro: string;
-  ibge: string;
-}
-
-interface contactProps {
-  name: string;
-  email: string;
-  date: string;
-  adress: adressProps;
-}
-
-interface contactListProps{
-  contactList: () => contactProps[];
-}
+import {
+  contactProps,
+  contactListProps,
+} from '../@types/contact';
 
 export const useContactList = create<contactListProps>((set) => ({
-  contactList: () => {
-    const storagedList = localStorage.getItem('@contact:list');
-
-    if (storagedList) {
-      return JSON.parse(storagedList);
-    }
-    return [];
+  contactList: [],
+  addContact:
+  (props: contactProps) => set((state: contactListProps) => ({
+    contactList: [...state.contactList, {
+      id: state.contactList.length + 1,
+      name: props.name,
+      email: props.email,
+      date: props.date,
+      adress: {
+        cep: props.adress.cep,
+        complemento: props.adress.complemento,
+        localidade: props.adress.localidade,
+        uf: props.adress.uf,
+        logradouro: props.adress.logradouro,
+        ibge: props.adress.ibge,
+      },
+    }],
+  })),
+  editingContact: {
+    id: 0,
+    name: '',
+    date: '',
+    email: '',
+    adress: {
+      cep: '',
+      complemento: '',
+      localidade: '',
+      uf: '',
+      logradouro: '',
+      ibge: '',
+    },
   },
+  setEditingContact: (props: contactProps) => set((state) => ({
+    editingContact: state.contactList.find((item) => item.id === props.id) || state.editingContact,
+  })),
+  updateContact: (props: contactProps) => set((state) => ({
+    contactList: state.contactList.map((item) => {
+      if (item.id === props.id) {
+        return props;
+      }
+      return item;
+    }),
+  })),
+  removeContact: (id: number) => set((state) => ({
+    contactList: state.contactList.filter((item) => item.id !== id),
+  })),
 }));
