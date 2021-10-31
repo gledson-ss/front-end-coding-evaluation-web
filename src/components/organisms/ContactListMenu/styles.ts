95b2e2aac28619b178f45cc6be81b82dec346d00
gledson-ss <github@gledson.dev>
2021-10-31 12:26:37 -0300

    refactor: update responsive

--- src/components/organisms/ContactListMenu/styles.ts
+++ src/components/organisms/ContactListMenu/styles.ts
@@ -5,15 +5,23 @@ export const Container = styled.menu`
   display: flex;
   justify-content: center;
   margin-top: -100px;
+  padding-bottom: 36px;
 `;
 
 export const Content = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  max-width: 1366px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  max-width: 1280px;
+  grid-gap: 36px;
+  width: 100%;
 
-  @media (max-width: 799px) {
+  @media (max-width: 1220px) {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+  @media (max-width: 832px) {
     justify-content: center;
   }
 `;
+export const CardContainer = styled.div``;
