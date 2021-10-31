a48d82a35dc8f0557ce08b73c43097864f12f62e
gledson-ss <github@gledson.dev>
2021-10-31 12:25:41 -0300

    feat: add state and configs

--- src/components/organisms/ContactListMenu/index.tsx
+++ src/components/organisms/ContactListMenu/index.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {
+  FC, useEffect, useState,
+} from 'react';
 
-import * as Styled from './styles';
+import { contactProps } from '../../../@types/contact';
+import { useContactList } from '../../../hooks/useContactList';
 import Contact from '../../molecules/Contact';
+import * as Styled from './styles';
 
-const ContactListMenu: React.FC = () => (
-  <Styled.Container>
-    <Styled.Content>
-      <Contact />
-      <Contact />
-      <Contact />
-    </Styled.Content>
+const ContactListMenu: FC = () => {
+  const { contactList } = useContactList((state) => state);
+  const [contacts, setContacts] = useState<contactProps[]>([]);
 
-  </Styled.Container>
-);
+  useEffect(() => {
+    setContacts([...contactList]);
+  }, [contactList]);
+
+  return (
+    <Styled.Container>
+
+      <Styled.Content>
+        {
+          contacts.map((item: contactProps) => (
+            <Styled.CardContainer>
+              <Contact key={item.name} listItem={item} />
+            </Styled.CardContainer>
+          ))
+        }
+      </Styled.Content>
+
+    </Styled.Container>
+  );
+};
 
 export default ContactListMenu;
