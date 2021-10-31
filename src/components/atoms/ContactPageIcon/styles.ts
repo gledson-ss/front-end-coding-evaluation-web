2a9632617a7fed5d58883241c52ad696cebaa277
gledson-ss <github@gledson.dev>
2021-10-31 12:57:07 -0300

    style: change order imports

--- src/components/atoms/ContactPageIcon/styles.ts
+++ src/components/atoms/ContactPageIcon/styles.ts
@@ -1,8 +1,9 @@
-import styled from 'styled-components';
 import { RiContactsFill } from 'react-icons/ri';
+import styled from 'styled-components';
 
 export const ContactPageIcon = styled(RiContactsFill)`
   color: ${(props) => props.theme.colors.orange};
   width: 80px;
   height: 80px;
+  margin-left: -7px;
 `;
