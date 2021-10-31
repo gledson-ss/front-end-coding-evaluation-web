5aef8079569ad66ba75672a88d1d3c57b88ca027
gledson-ss <github@gledson.dev>
2021-10-31 12:21:38 -0300

    feat: adapt edit and create modal

--- src/components/organisms/MenuModal/index.tsx
+++ src/components/organisms/MenuModal/index.tsx
@@ -1,23 +1,29 @@
 import React, { FC, ReactNode, useEffect } from 'react';
-import * as Styled from './styles';
+
 import { useModal } from '../../../hooks/useModal';
+import * as Styled from './styles';
 
 interface ModalProps {
   children: ReactNode
 }
 
 const MenuModal: FC<ModalProps> = ({ children }) => {
-  const { modalState, setIsOpen } = useModal((state) => state);
-  function handleOpenModal() {
-    setIsOpen(false);
+  const {
+    modalCreatelIsOpen,
+    modalEditlIsOpen,
+    setModalCreateIsOpen,
+    setModalEditIsOpen,
+  } = useModal((state) => state);
+  function handleCloseModal() {
+    setModalCreateIsOpen(false);
+    setModalEditIsOpen(false);
   }
 
   return (
-
     <Styled.Container
-      onEscapeKeydown={() => handleOpenModal()}
-      onBackgroundClick={() => handleOpenModal()}
-      isOpen={modalState.isOpen}
+      onEscapeKeydown={() => handleCloseModal()}
+      onBackgroundClick={() => handleCloseModal()}
+      isOpen={modalCreatelIsOpen || modalEditlIsOpen}
     >
       { children }
     </Styled.Container>
