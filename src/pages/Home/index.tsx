03118fc97a3fcd596973c13108145dd0c615f2a2
gledson-ss <github@gledson.dev>
2021-10-31 12:03:16 -0300

    style: change spaces

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,6 @@ import React, { FC } from 'react';
 
 import HomeSection from '../../components/templates/HomeSection';
 
-const Home: FC = () => (
-  <>
-    <HomeSection />
-  </>
-);
+const Home: FC = () => <HomeSection />;
 
 export default Home;
