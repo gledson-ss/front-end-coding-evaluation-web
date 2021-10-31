06ff68ea0211890af89935324bc764ac701ae11b
gledson-ss <github@gledson.dev>
2021-10-31 12:15:12 -0300

    refactor: update HomeSection

--- src/components/templates/HomeSection/index.tsx
+++ src/components/templates/HomeSection/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
+import ContactListMenu from '../../organisms/ContactListMenu';
 import Header from '../../organisms/Header';
 import ModalAddContact from '../../organisms/ModalAddContact';
-import ContactListMenu from '../../organisms/ContactListMenu';
+import ModalEditContact from '../../organisms/ModalEditContact';
+import * as Styled from './styles';
 
 const HomeSection: React.FC = () => (
-  <>
+  <Styled.Container>
     <Header />
     <ContactListMenu />
     <ModalAddContact />
-  </>
+    <ModalEditContact />
+  </Styled.Container>
 );
 
 export default HomeSection;
