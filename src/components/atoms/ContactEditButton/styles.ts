a8f3075a7f7af6bdf85f4d08c9602334acf1cff7
gledson-ss <github@gledson.dev>
2021-10-31 12:58:18 -0300

    feat: edit button update

---
+++ src/components/atoms/ContactEditButton/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ButtonIcon = styled.button`
+  background-color: ${(props) => props.theme.colors.blue};
+  color: ${(props) => props.theme.colors.white};
+  border: none;
+  &:focus {
+    outline: none;
+  }
+  width: 45px;
+  height: 45px;
+
+  border-radius: 8px;
+  &:hover {
+    cursor: pointer;
+  }
+  transition: filter 0.3s;
+  :hover {
+    filter: brightness(0.7);
+  }
+`;
