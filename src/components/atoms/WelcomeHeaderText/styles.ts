0790bc7bbbac2f5305e66419eb8bf70b41173a9c
gledson-ss <github@gledson.dev>
2021-10-31 12:45:15 -0300

    refactor: update responsive

--- src/components/atoms/WelcomeHeaderText/styles.ts
+++ src/components/atoms/WelcomeHeaderText/styles.ts
@@ -2,4 +2,7 @@ import styled from 'styled-components';
 
 export const WelcomeHeaderText = styled.h1`
   color: ${(props) => props.theme.colors.greyLight};
+  @media (max-width: 250px) {
+    display: none;
+  }
 `;
